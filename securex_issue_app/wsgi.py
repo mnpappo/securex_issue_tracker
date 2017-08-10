2d3bf772aba63e523137ba057521a8bf21704b93
Md. Nadimozzaman Pappo <mnpappo@gmail.com>
2017-08-10 20:16:00 +0600

    dj project init

---
+++ securex_issue_app/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for securex_issue_app project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "securex_issue_app.settings")
+
+application = get_wsgi_application()
